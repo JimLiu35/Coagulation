00b99ceb96d3e1d0bba545da7dfe766c3f3a9789
JimLiu35 <gliu35@jhu.edu>
2023-08-17 11:11:19 -0400

    download raw code from SYBORGS github

--- raw_code/DetermineDelayTherapy.m
+++ raw_code/DetermineDelayTherapy.m
@@ -1,23 +1,30 @@
 function Delay = DetermineDelayTherapy(Tcheck,MeasuredThrombin)
+
 %first, look for the first data point below half of the thrombin peak
+
 [MaxThrombin,MaxThrombinIndex] = max(MeasuredThrombin);
 TimeMaxThrombin = Tcheck(MaxThrombinIndex);
+
 SlopeUpperValueIndex = find(MeasuredThrombin<=0.5*MaxThrombin & Tcheck<=TimeMaxThrombin,1,'last');
 SlopeUpperValue = MeasuredThrombin(SlopeUpperValueIndex);
 SlopeUpperTime = Tcheck(SlopeUpperValueIndex);
+
 %calculate the slope with the next lower data point
 SlopeLowerValue = MeasuredThrombin(SlopeUpperValueIndex-1);
 SlopeLowerTime = Tcheck(SlopeUpperValueIndex-1);
 Slope = (SlopeUpperValue - SlopeLowerValue)/(SlopeUpperTime-SlopeLowerTime);
+
 %calculate the x-intercept
 b = SlopeLowerValue-Slope*SlopeLowerTime;
 xintercept = -b/Slope;
 
 if xintercept > 0
     EstimatedThrombinAtIntercept = interp1(Tcheck,MeasuredThrombin,xintercept);
+
     %calculate parameters for perpendicalar through this x-intercept
     SlopePerp = -1/Slope;
     bPerp = -SlopePerp*xintercept;
+
     %find a pair of points backwards from the x-intercept that lie on either
     %side of this perpendicular
     found = 0;
@@ -25,9 +32,11 @@ if xintercept > 0
     while found == 0
         CurrentThrombinValue=MeasuredThrombin(CurrentIndex);
         CurrentPerpThrombinValue = SlopePerp*Tcheck(CurrentIndex)+bPerp;
+
         if CurrentIndex > 1
             NextThrombinValue = MeasuredThrombin(CurrentIndex-1);
             NextPerpThrombinValue = SlopePerp*Tcheck(CurrentIndex-1)+bPerp;
+
             if(CurrentThrombinValue <= CurrentPerpThrombinValue && NextThrombinValue < NextPerpThrombinValue)
                 found = 1;
                 Delay = Tcheck(CurrentIndex);
@@ -45,5 +54,5 @@ if xintercept > 0
 else
     Delay = Tcheck(1);
 end
+
 return
-end
